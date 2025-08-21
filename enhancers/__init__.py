628edb3c683e3e4abf577c71e52b48f211bd9649
liningping <728359849@qq.com>
2025-08-21 07:38:08 +0000

    feat: add enhancer registry

---
+++ enhancers/__init__.py
@@ -0,0 +1,17 @@
+from .enhancer_registry import (
+    ENHANCER_REGISTRY,
+    get_enhancer,
+    get_enhancer_class,
+    get_supported_modalities,
+    list_registered_enhancers,
+    register_enhancer,
+)
+
+__all__ = [
+    "ENHANCER_REGISTRY",
+    "get_enhancer",
+    "get_enhancer_class",
+    "get_supported_modalities",
+    "list_registered_enhancers",
+    "register_enhancer",
+]
